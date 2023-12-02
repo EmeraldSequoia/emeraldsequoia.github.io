12041cfb91e10891c0a157f408a912ce0aa2deab
Steve Pucci <slpucci@users.noreply.github.com>
2023-12-02 15:55:02 -0800

    Delete index.php
    PHP is not supported anyway, and this was just a redirect

--- index.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<html>
-  <head>
-    <title>Emerald Sequoia</title>
-    <meta http-equiv="REFRESH" content="0;url=index.html">
-    </head>
-<body>
-</body>
-</html>
